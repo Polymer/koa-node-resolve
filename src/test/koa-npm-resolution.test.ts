f1fe49cb2411e14185d18f5fbe6d6c98f427a75e
Brendan Baldwin <brendan@usergenic.com>
2019-05-15 15:48:32 -0700

    Added copyright headers to typescript files.

--- src/test/koa-npm-resolution.test.ts
+++ src/test/koa-npm-resolution.test.ts
@@ -1,9 +1,21 @@
+/**
+ * @license
+ * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 import {resolve as resolvePath} from 'path';
 import request from 'supertest';
 import test from 'tape';
 
 import createMiddleware from '../koa-npm-resolution';
-
 import {createAndServe, squeezeHTML} from './test-utils';
 
 test('transforms resolvable specifier in JavaScript module', async (t) => {

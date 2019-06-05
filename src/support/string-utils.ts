f04d151809fd33463095c7817ffd7429c718daab
Brendan Baldwin <brendan@usergenic.com>
2019-06-04 17:27:06 -0700

    I made a bad regexp and @aomarks found it! Thanks!

--- src/support/string-utils.ts
+++ src/support/string-utils.ts
@@ -13,4 +13,4 @@
  */
 export const preserveSurroundingWhitespace =
     (original: string, target: string): string => original.match(/^\s*/)![0] +
-    target.replace(/^\s*|\s*$/, '') + original.match(/\s*$/)![0];
+    target.replace(/^\s*|\s*$/g, '') + original.match(/\s*$/)![0];

5b02b20830e6c0206fa36bb13519fdc4b22c2195
Brendan Baldwin <brendan@usergenic.com>
2019-06-07 16:08:28 -0700

    Added logLevel to dynamicImport test.

--- src/test/koa-module-specifier-transform.test.ts
+++ src/test/koa-module-specifier-transform.test.ts
@@ -71,7 +71,7 @@ test('moduleSpecifierTransform will convert dynamic imports', async (t) => {
       {
         middleware: [moduleSpecifierTransform(
             (_baseURL, specifier) => `./node_modules/${specifier}/index.js`,
-            {logger})],
+            {logger, logLevel: 'info'})],
         routes: {
           '/my-module.js': `
             import('x').then((x) => x.doStuff());
